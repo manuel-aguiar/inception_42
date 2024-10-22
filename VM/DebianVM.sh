dc2b93511625d6fccdd5e17de7b40cd825c76c00
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-10-22 15:15:47 +0000

    random message

--- VM/DebianVM.sh
+++ VM/DebianVM.sh
@@ -24,8 +24,8 @@
 
 #############       NAMES       ##############
 VM_NAME="myVM"
-VM_LOCATION="/$(whoami)"
-VM_OS_LINK="https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-12.2.0-amd64-netinst.iso"
+VM_LOCATION="/workspaces/inception_42/VM"
+VM_OS_LINK="https://cdimage.debian.org/mirror/cdimage/archive/12.2.0/amd64/iso-cd/debian-12.2.0-amd64-netinst.iso"
 VM_OS_VIRTUALBOX_NAME="Debian 64"
 
 #############       SPECS       ##############
@@ -70,15 +70,15 @@ mkdir -p "$VM_LOCATION"
 
 if [ ! -f "$VM_OS_PATH" ]; then
     echo "File $VM_OS_IMAGENAME not found. Downloading....."
-    wget -O -c "$VM_OS_PATH" "$VM_OS_LINK"
+    wget -O "$VM_OS_PATH" "$VM_OS_LINK"
 else
     echo "File $VM_OS_IMAGENAME already exists in target directory, skipping download."
 fi
 
-
+touch "$VM_PRESEED_PATH"
 
 # create the preseed file for debian autoconfiguration
-cat <<-EOF > "$VM_PRESEED_PATH"
+cat << EOF > "$VM_PRESEED_PATH"
 
 d-i debian-installer/language string ${DEBIAN_LANG}
 d-i debian-installer/country string ${DEBIAN_COUNTRY}
@@ -87,12 +87,12 @@ d-i debian-installer/locale string ${DEBIAN_LOCALE}
 d-i console-setup/ask_detect boolean false
 d-i keyboard-configuration/xkb-keymap select ${DEBIAN_KEYBOARD}
 
-#                                                   Network configuration
+# Network configuration
 d-i netcfg/choose_interface select auto
 d-i netcfg/get_hostname string unassigned-hostname
 d-i netcfg/get_domain string unassigned-domain
 
-#                                                   Setup the sudo user
+# Setup the sudo user
 d-i passwd/root-password password ${DEBIAN_SUDO_PASS}
 d-i passwd/root-password-again password ${DEBIAN_SUDO_PASS}
 
@@ -117,18 +117,18 @@ EOF
 
 # Create the actual VM
 VBoxManage createvm --name "$VM_NAME" -ostype "$VM_OS_VIRTUALBOX_NAME" --register --basefolder "$VM_LOCATION"
-VBoxManage modifyvm "$VM_NAME" --ioapic on                     
+VBoxManage modifyvm "$VM_NAME" --ioapic on
 VBoxManage modifyvm "$VM_NAME" --memory "$VM_RAM" --vram "$VM_VRAM" --cpus "$VM_CPU_COUNT"
 VBoxManage modifyvm "$VM_NAME" --nic1 nat
 
 
-VBoxManage createhd --filename "$VM_LOCATION"/"$VM_NAME"/"$VM_NAME_DISK" --size "$VM_HD_SIZE" --format VDI                     
-VBoxManage storagectl "$VM_NAME" --name "SATA Controller" --add sata --controller IntelAhci       
-VBoxManage storageattach "$VM_NAME" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium  "$VM_LOCATION"/"$VM_NAME"/"$VM_NAME_DISK"              
-VBoxManage storagectl "$VM_NAME" --name "IDE Controller" --add ide --controller PIIX4       
+VBoxManage createhd --filename "$VM_LOCATION"/"$VM_NAME"/"$VM_NAME_DISK" --size "$VM_HD_SIZE" --format VDI
+VBoxManage storagectl "$VM_NAME" --name "SATA Controller" --add sata --controller IntelAhci
+VBoxManage storageattach "$VM_NAME" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium  "$VM_LOCATION"/"$VM_NAME"/"$VM_NAME_DISK"
+VBoxManage storagectl "$VM_NAME" --name "IDE Controller" --add ide --controller PIIX4
 VBoxManage storageattach "$VM_NAME" --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium "$VM_OS_PATH"
-VBoxManage storageattach "$VM_NAME" --storagectl "IDE Controller" --port 0 --device 1 --type dvddrive --medium "$VM_PRESEED_PATH"       
-VBoxManage modifyvm "$VM_NAME" --boot1 dvd --boot2 disk --boot3 none --boot4 none                                                                 
+VBoxManage storageattach "$VM_NAME" --storagectl "IDE Controller" --port 0 --device 1 --type dvddrive --medium "$VM_PRESEED_PATH"
+VBoxManage modifyvm "$VM_NAME" --boot1 dvd --boot2 disk --boot3 none --boot4 none
 
 
 VBoxManage modifyvm "$VM_NAME" --nictype1 82540EM
@@ -138,4 +138,4 @@ VBoxManage modifyvm "$VM_NAME" --bridgeadapter1 eno2
 
 #remove the preseed file, not needed anymore
 rm -rf "$VM_PRESEED_PATH"
-
+rm -rf "$VM_OS_PATH"
