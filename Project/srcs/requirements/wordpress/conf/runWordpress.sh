b6bd11533f8d92837ec0c9d791297dae2541f8d2
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-11-04 10:18:31 +0000

    random message

--- Project/srcs/requirements/wordpress/conf/runWordpress.sh
+++ Project/srcs/requirements/wordpress/conf/runWordpress.sh
@@ -4,28 +4,26 @@
 php_version=$(php -v | head -n 1 | cut -d ' ' -f 2 | cut -d '.' -f -2)
 php_fpm=php-fpm$php_version
 
-
-
 if [ ! -f wp-config.php ];
 then
 	wp core download --allow-root
 
 	wp config create --allow-root \
-					 --dbname=$SQL_DB \
-					 --dbuser=$SQL_USER \
-					 --dbpass=$SQL_PASS \
-					 --dbhost=mariadb
+					 --dbname=$MYSQL_DATABASE_FILE \
+					 --dbuser=$MYSQL_USER_FILE \
+					 --dbpass=$MYSQL_PASSWORD_FILE \
+					 --dbhost="${HOSTNAME_DB}:${PORT_LISTEN_DATABASE}"
 
 	wp core install  --allow-root \
 					 --skip-email \
-					 --title="inception" \
-					 --url=$WP_ADM_URL \
-					 --admin_user=$WP_ADM_USER \
-					 --admin_password=$WP_ADM_PASS \
-					 --admin_email=$WP_ADM_MAIL
+					 --title=${WEBSITE_TITLE} \
+					 --url=${DOMAIN_NAME} \
+					 --admin_user=$WP_ADMIN_USER_FILE \
+					 --admin_password=$WP_ADMIN_PASS_FILE \
+					 --admin_email=$WP_ADMIN_MAIL_FILE
 
-	wp user create   --allow-root $WP_USER $WP_MAIL \
-					 --user_pass=$WP_PASS
+	wp user create   --allow-root $WP_USER_USER_FILE $WP_USER_MAIL_FILE \
+					 --user_pass=$WP_USER_PASS_FILE
 fi
 
 /usr/sbin/$php_fpm -F
