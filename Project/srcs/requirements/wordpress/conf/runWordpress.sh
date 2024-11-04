c4bb39a27526dc415e6bac4e62fbd559d99966f5
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-11-04 09:06:29 +0000

    random message

--- Project/srcs/requirements/wordpress/conf/runWordpress.sh
+++ Project/srcs/requirements/wordpress/conf/runWordpress.sh
@@ -2,4 +2,30 @@
 
 
 php_version=$(php -v | head -n 1 | cut -d ' ' -f 2 | cut -d '.' -f -2)
-php_fpm=php$php_version-fpm
+php_fpm=php-fpm$php_version
+
+
+
+if [ ! -f wp-config.php ];
+then
+	wp core download --allow-root
+
+	wp config create --allow-root \
+					 --dbname=$SQL_DB \
+					 --dbuser=$SQL_USER \
+					 --dbpass=$SQL_PASS \
+					 --dbhost=mariadb
+
+	wp core install  --allow-root \
+					 --skip-email \
+					 --title="inception" \
+					 --url=$WP_ADM_URL \
+					 --admin_user=$WP_ADM_USER \
+					 --admin_password=$WP_ADM_PASS \
+					 --admin_email=$WP_ADM_MAIL
+
+	wp user create   --allow-root $WP_USER $WP_MAIL \
+					 --user_pass=$WP_PASS
+fi
+
+/usr/sbin/$php_fpm -F
