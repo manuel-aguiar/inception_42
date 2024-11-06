d8d751b84d56aa13e03922c745dd2ecfa6fc7c13
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-11-06 15:14:06 +0000

    random message

--- Project/srcs/requirements/redis-cache/tools/runRedisCache.sh
+++ Project/srcs/requirements/redis-cache/tools/runRedisCache.sh
@@ -6,8 +6,8 @@
 #    By: codespace <codespace@student.42.fr>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/11/05 14:44:53 by codespace         #+#    #+#              #
-#    Updated: 2024/11/05 14:50:04 by codespace        ###   ########.fr        #
+#    Updated: 2024/11/06 15:10:54 by codespace        ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-exec /usr/bin/redis-server --bind "0.0.0.0" --port ${PORT_LISTEN_DATABASE} --protected-mode "no" --daemonize "no"
+exec /usr/bin/redis-server --bind "0.0.0.0" --port ${PORT_LISTEN_REDIS} --protected-mode "no" --daemonize "no"
